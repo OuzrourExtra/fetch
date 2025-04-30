e3ef5fb9f3f4ac055fa6652adbee848a1e9ff1f9
Ilyas Ouzrour <ilyas.ouzrour@gmail.com>
2025-04-30 21:44:23 +0100

    add the fetch with concurrency

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ package main
 import(
 	"fmt"
 	"io"
+	"time"
 	"net/http"
 	"os"
 )
-
-func main(){
-	for _,url := range os.Args[1:]{
+func WithoutConcurrency(arguments []string){
+	start:=time.Now()
+	for _,url := range arguments{
 		resp , err := http.Get(url)
 		if err!=nil{
 			fmt.Fprintf(os.Stderr,"fetch : %v\n",err)
@@ -22,4 +23,37 @@ func main(){
 		}
 		fmt.Printf("%s",b)
 	}
+	fmt.Printf("%fs elapsed\n",time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan <- string){
+	start:=time.Now()
+	resp,err := http.Get(url)
+	if err!=nil{
+		ch <- fmt.Sprint(err)
+		return
+	}
+	html , err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err!=nil{
+		ch <- fmt.Sprintf("while reading %s : %v",url,err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%fs \t %s \t %s ",secs,url,html)
+}
+func WithConcurrency(arguments []string){
+	start := time.Now()
+	ch := make(chan string)
+	for _ , url := range arguments{
+		go fetch(url,ch)
+	}
+	for range arguments {
+		fmt.Println(<-ch) // wait for each fetch to send a result
+	}
+	fmt.Printf("%fs elapsed\n",time.Since(start).Seconds())
+}
+func main(){
+	WithoutConcurrency(os.Args[1:])
+	WithConcurrency(os.Args[1:])
 }
